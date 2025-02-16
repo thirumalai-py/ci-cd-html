2b1761860d6b36d2e3ab5d4516e4ec15fa660d5d
Thirumalai Raja <webykart.com@gmail.com>
2025-02-16 20:26:38 +0530

    Get the Git repo last commit

--- script.py
+++ script.py
@@ -1,13 +1,18 @@
 from github import *
+from dotenv import load_dotenv
+import os
+ 
+# Load environment variables from .env file
+load_dotenv()
 
-GIT_TOKEN = "secret"
+GIT_TOKEN = os.getenv("GIT_TOKEN")
 
-# Auth the Repot
-
-# using an access token
-auth = Auth.Token(GIT_TOKEN)
-
+# print(GIT_TOKEN)
+# Auth the Repo
 github_access = Github(GIT_TOKEN)
+
+user = github_access.get_user()
+print("Authenticated as:", user.login)
 
 # Get Repo Access
 repo = github_access.get_repo("thirumalai-py/ci-cd-html")
@@ -15,3 +20,15 @@ repo = github_access.get_repo("thirumalai-py/ci-cd-html")
 # Get repository details
 print(repo.full_name)
 print(repo.description)
+
+# Get commits
+commits = repo.get_commits()
+
+# Get Total count
+# print(commits.totalCount)
+
+# print("Latest commit: ", commits[0])
+
+latest_commit = commits[0]
+
+print(latest_commit.sha)
